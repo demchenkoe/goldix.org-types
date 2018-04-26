9afff72ef45e17f7a6747fe59f7327a9752c73f5
Demchenko Evgeny <demchenko@inf.ai>
2018-04-26 22:39:29 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Validator } = require('@goldix.org/validator');
+const { CompositeType } = require('./lib/CompositeType');
+const { Type } = require('./lib/Type');
+
+
+module.exports = { Validator, CompositeType, Type };
